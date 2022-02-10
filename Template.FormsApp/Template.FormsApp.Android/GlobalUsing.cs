a853b63527fe260fe6af582f0e1984e61584841e
usausa <machi.pon@gmail.com>
2022-02-10 10:21:42 +0900

    Update global using

--- Template.FormsApp/Template.FormsApp.Android/GlobalUsing.cs
+++ Template.FormsApp/Template.FormsApp.Android/GlobalUsing.cs
@@ -4,12 +4,16 @@ global using System.Collections;
 global using System.Collections.Generic;
 global using System.Collections.ObjectModel;
 global using System.ComponentModel;
-global using System.Data.Common;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Globalization;
+global using System.Linq;
+global using System.Reactive.Disposables;
+global using System.Reactive.Linq;
+global using System.Reactive.Subjects;
 global using System.Threading;
 global using System.Threading.Tasks;
-global using System.Windows.Input;
 
-
+global using Smart;
+global using Smart.Collections.Generic;
 global using Smart.ComponentModel;
+global using Smart.Text;
--- Template.FormsApp/Template.FormsApp/GlobalUsing.cs
+++ Template.FormsApp/Template.FormsApp/GlobalUsing.cs
@@ -7,10 +7,15 @@ global using System.ComponentModel;
 global using System.Data.Common;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Globalization;
+global using System.Linq;
+global using System.Reactive.Disposables;
+global using System.Reactive.Linq;
+global using System.Reactive.Subjects;
 global using System.Threading;
 global using System.Threading.Tasks;
 global using System.Windows.Input;
 
+global using Smart;
 global using Smart.Collections.Generic;
 global using Smart.ComponentModel;
 global using Smart.Forms.Messaging;
@@ -19,6 +24,7 @@ global using Smart.Navigation;
 global using Smart.Navigation.Attributes;
 global using Smart.Navigation.Plugins.Parameter;
 global using Smart.Navigation.Plugins.Scope;
+global using Smart.Text;
 
 global using Template.FormsApp.Messaging;
 global using Template.FormsApp.Models;
